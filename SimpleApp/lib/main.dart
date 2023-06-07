344e8dc9ce5d15bbbc3abde8fdeb446776df6049
Chingiz Panahli <chingiz.panahli@gmail.com>
2023-06-07 21:13:34 +0400

    Implement a list of shopping card

--- SimpleApp/lib/main.dart
+++ SimpleApp/lib/main.dart
@@ -1,139 +1,123 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: "Flutter App",
-        theme: ThemeData(
-            colorScheme: const ColorScheme(
-                brightness: Brightness.light,
-                primary: Colors.pink,
-                onPrimary: Colors.white,
-                secondary: Colors.pink,
-                onSecondary: Colors.white,
-                error: Colors.red,
-                onError: Colors.white,
-                background: Colors.white,
-                onBackground: Colors.black,
-                surface: Colors.white,
-                onSurface: Colors.black),
-            iconTheme:
-                const IconThemeData(size: 38, color: Colors.pink, weight: 300),
-            textTheme: const TextTheme(
-                bodyMedium: TextStyle(
-                    fontFamily: 'Arial', fontSize: 24, color: Colors.black))),
-        home: const MyHomePage());
+    return const MaterialApp(
+      home: MyHomePage(title: "Category promotion emails"),
+    );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+  final String title;
+  const MyHomePage({super.key, this.title = ''});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  double _loop = 4;
-  bool isAmber = false;
-  Timer? timer;
-  @override
-  void initState() {
-    super.initState();
-    timer = Timer.periodic(Duration(seconds: _loop.toInt()), (Timer timer) {
-      setState(() {
-        isAmber = !isAmber;
-      });
-    });
-  }
-
-  _changeTimer() {
-    timer!.cancel();
-    timer = Timer.periodic(Duration(seconds: _loop.toInt()), (timer) {
-      setState(() {
-        isAmber = !isAmber;
-      });
-    });
-  }
-
+  Map<String, bool?> checkLists = {
+    'Groceries': true,
+    'Kids': false,
+    'Personal': true
+  };
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: isAmber ? Colors.amber : Colors.green,
-      appBar: AppBar(
-        title: const Text("Flutter App"),
-        centerTitle: true,
-      ),
-      body: Column(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Column(children: [
+          Container(
+              padding: const EdgeInsets.all(16),
+              child: const Text(
+                "Shopping Categories",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black54),
+              )),
+          Expanded(
+              child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              children: checkLists.keys
+                  .map((taskName) => CheckboxListTile(
+                        title: Text(taskName),
+                        value: checkLists[taskName],
+                        onChanged: (bool? value) {
+                          setState(() {
+                            checkLists[taskName] = value;
+                          });
+                        },
+                      ))
+                  .toList(),
+            ),
+          )),
+          Container(
+              padding: const EdgeInsets.all(16),
+              child: const Text(
+                "Shopping Icons",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black54),
+              )),
+          Expanded(
+              flex: 1,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: checkLists.keys
+                      .map((taskName) => ShoppingCard(
+                          chore: taskName, on: checkLists[taskName]!))
+                      .toList(),
+                ),
+              ))
+        ]));
+  }
+}
+
+class ShoppingCard extends StatelessWidget {
+  final String chore;
+  final bool on;
+  const ShoppingCard({super.key, this.chore = '', this.on = true});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          border: Border.all(color: const Color(0xffeeeeee), width: 2.0),
+          color: Colors.white38,
+          borderRadius: const BorderRadius.all(Radius.circular(20))),
+      margin: const EdgeInsets.all(8),
+      height: 200,
+      width: 200,
+      child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Container(
-            height: 150,
-            width: 150,
-            padding: const EdgeInsets.all(30),
-            margin: const EdgeInsets.all(20),
-            decoration: const BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.pink,
+          Center(
+            child: Icon(
+              Icons.shopping_bag_outlined,
+              size: 100,
+              color: on ? Colors.green : Colors.grey,
             ),
-            child: (const FlutterLogo()),
           ),
-          Slider(
-              value: _loop,
-              min: 0,
-              max: 15,
-              activeColor: Colors.black,
-              divisions: 15,
-              onChanged: (value) {
-                setState(() {
-                  _loop = value;
-                  _changeTimer();
-                });
-              }),
-          Text(_loop.toString()),
-          const Expanded(
-            child: Center(
-              child: Text("Welcome to Flutter"),
+          Text(
+            chore,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 24,
+              color: on ? Colors.green : Colors.grey,
             ),
           )
         ],
-      ),
-      drawer: const Drawer(
-        child: Center(
-          child: Text("My Profile"),
-        ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            IconButton(
-                onPressed: () {
-                  debugPrint("Home button clicked");
-                },
-                icon: const Icon(Icons.home)),
-            IconButton(
-                onPressed: () {
-                  debugPrint("Search button clicked");
-                },
-                icon: const Icon(Icons.search)),
-            IconButton(
-                onPressed: () {
-                  debugPrint("Settings button clicked");
-                },
-                icon: const Icon(
-                  Icons.settings,
-                )),
-          ],
-        ),
       ),
     );
   }
